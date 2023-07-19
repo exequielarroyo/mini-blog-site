29f323c107189e71a99d70280d064145fe04204d
Exequiel Arroyo <zekielarroyo@gmail.com>
2023-07-19 15:36:30 +0800

    Removed Unused lines

--- config/database.php
+++ config/database.php
@@ -1,9 +1,4 @@
 <?php
 $connection = mysqli_connect('localhost', 'root', null, 'mini_blog_site');
 
-// if ($connection) {
-//     echo "good connection";
-// } else {
-//     echo "error";
-// }
 ?>
